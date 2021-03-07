45ce369c81feb154d7fef97d0f30a2fe3e672b52
patryktomaszewski <58734528+patryktomaszewski@users.noreply.github.com>
2021-03-07 17:58:20 +0100

    test basket

--- ecommerce-store/store/tests/test_views.py
+++ ecommerce-store/store/tests/test_views.py
@@ -1,9 +1,10 @@
 # from unittest import skip
-
+from importlib import import_module
 from django.contrib.auth.models import User
 from django.http import HttpRequest
-from django.test import Client, RequestFactory, TestCase
+from django.test import Client, TestCase
 from django.urls import reverse
+from django.conf import settings
 
 from store.models import Category, Product
 from store.views import product_all
@@ -16,7 +17,6 @@ from store.views import product_all
 
 class TestViewResponses(TestCase):
     def setUp(self):
-        self.factory = RequestFactory()
         self.c = Client()
         Category.objects.create(name='django', slug='django')
         User.objects.create(username='admin')
@@ -65,16 +65,12 @@ class TestViewResponses(TestCase):
 
     def test_homepage_html(self):
         request = HttpRequest()
+        engine = import_module(settings.SESSION_ENGINE)
+        request.session = engine.SessionStore()
         response = product_all(request)
         html = response.content.decode('utf8')
         self.assertIn('<title>BookStore</title>', html)
         self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
         self.assertEqual(response.status_code, 200)
 
-    def test_view_function(self):
-        request = self.factory.get('/django-beginners')
-        response = product_all(request)
-        html = response.content.decode('utf8')
-        self.assertIn('<title>BookStore</title>', html)
-        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
-        self.assertEqual(response.status_code, 200)
+ 

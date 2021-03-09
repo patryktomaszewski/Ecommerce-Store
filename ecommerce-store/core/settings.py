d43ee35b9f69fa533c4b07474bb51d7b1d505004
patryktomaszewski <58734528+patryktomaszewski@users.noreply.github.com>
2021-03-09 16:07:42 +0100

    crating custom user model

--- ecommerce-store/core/settings.py
+++ ecommerce-store/core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'store',
     'basket',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,8 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+#Custom user model
+AUTH_USER_MODEL = 'account.UserBase'
+LOGIN_REDIRECT_URL = '/account/dashboard'
+LOGIN_URL = '/account/login/'

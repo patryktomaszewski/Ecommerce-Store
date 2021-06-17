9516572cbca49d2f45b02de31420556525e828ed
patryktomaszewski <58734528+patryktomaszewski@users.noreply.github.com>
2021-06-17 11:58:49 +0200

    password reset

--- ecommerce-store/account/urls.py
+++ ecommerce-store/account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from . import views
 from django.contrib.auth import views as auth_views
-from .forms import (UserLoginForm)
+from .forms import (UserLoginForm, PwdResetForm, PwdResetConfirmForm)
 from django.views.generic import TemplateView
 
 app_name = 'account'
@@ -12,6 +12,18 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='/account/login/'), name='logout'),
     path('register/', views.account_register, name='register'),
     path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="account/user/password_reset_form.html",
+                                                                 success_url='password_reset_email_confirm',
+                                                                 email_template_name='account/user/password_reset_email.html',
+                                                                 form_class=PwdResetForm), name='pwdreset'),
+    path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='account/user/password_reset_confirm.html',
+                                                                                                success_url='password_reset_complete/',
+                                                                                                form_class=PwdResetConfirmForm),
+        name="password_reset_confirm"),
+    path('password_reset/password_reset_email_confirm/',
+         TemplateView.as_view(template_name="account/user/reset_status.html"), name='password_reset_done'),
+    path('password_reset_confirm/Mg/password_reset_complete/',
+         TemplateView.as_view(template_name="account/user/reset_status.html"), name='password_reset_complete'),
     # user dashboard
     path('dashboard/', views.dashboard, name='dashboard'),
     path('profile/edit/', views.edit_details, name='edit_details'),

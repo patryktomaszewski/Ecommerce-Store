097e454e26a84bb056eeeb213bf158f410732648
patryktomaszewski <58734528+patryktomaszewski@users.noreply.github.com>
2021-06-18 13:45:55 +0200

    stripe elements

---
+++ ecommerce-store/payment/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from basket.basket import Basket
+import stripe
+
+
+@login_required
+def BasketView(request):
+    basket = Basket(request)
+    total = str(basket.get_total_price())
+    total = total.replace('.', '')
+    total = int(total)
+
+    stripe.api_key = ''
+    intent = stripe.PaymentIntent.create(
+        amount=total,
+        currency='gbp',
+        metadata={'userid': request.user.id}
+    )
+
+    return render(request, 'payment/home.html', {'client_secret': intent.client_secret})
